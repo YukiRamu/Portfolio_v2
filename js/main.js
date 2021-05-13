2da2a955e4e3de9b6566751df9ef36b204ecd159
Yuki <yukipooh4120@gmail.com>
2021-05-13 13:34:57 -0700

    as of May 13 13;00

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@ const openingSection = document.querySelector(".opening");
 const typingText = document.querySelector(".typingText");
 const text = "Once upon a time there was a passionate front-end developer who loved creating innovative websites, making people smile and happy. This is a story of her life."; //160 char
 const arrow = document.querySelector(".arrow");
-const intro = document.querySelector(".intro");
-const photo = document.querySelector(".photo");
-const fullProf = document.querySelector(".fullProf");
-const viewFullProfBtn = document.querySelector(".viewFullProfBtn");
-const backToAboutBtn = document.querySelector(".backToAboutBtn");
 
 //nav bar
 const navBar = document.querySelector(".nav");
 let navBarHeight = navBar.clientHeight;
-console.log(navBarHeight)
 
 //about
 const aboutSection = document.querySelector(".about");
+const introduction = document.querySelector(".indroduction");
+const fullProf = document.querySelector(".fullProf");
+const intro = document.querySelector(".intro");
+const photo = document.querySelector(".photo");
+const viewFullProfBtn = document.querySelector(".viewFullProfBtn");
+const backToAboutBtn = document.querySelector(".backToAboutBtn");
 
 //skills
 const skillSection = document.querySelector(".skills");
@@ -76,8 +76,14 @@ window.addEventListener("DOMContentLoaded", () => {
 //"see full profile" button clicked
 viewFullProfBtn.addEventListener("click", () => {
   smoothScroll("fullProf");
-  navBar.style.opacity = "0";
-
+  //hide all sections
+  navBar.style.display = "none";
+  openingSection.style.display = "none";
+  // fullProf.style.display = "block";
+  skillSection.style.display = "none";
+  workSection.style.display = "none";
+  contactSection.style.display = "none";
+  //slide
   intro.style.transform = "translateX(165%)";
   intro.style.opacity = "0";
   intro.style.transition = "1s linear";
@@ -91,7 +97,14 @@ viewFullProfBtn.addEventListener("click", () => {
 //"Back" buton clicked
 backToAboutBtn.addEventListener("click", () => {
   smoothScroll("intro");
-  navBar.style.opacity = "1";
+  //show all sections
+  //navBar.style.opacity = "1";
+
+  // openingSection.style.display = "block";
+  // skillSection.style.display = "flex";
+  // workSection.style.display = "flex";
+  // contactSection.style.display = "flex";
+  //slide back to about sections
   intro.style.transform = "translateX(-1%)";
   intro.style.opacity = "1";
   intro.style.transition = "1s linear";
